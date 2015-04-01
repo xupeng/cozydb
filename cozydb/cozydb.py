f928c7418151b7bcc8ad83128c79d692b58c4eb8
xupeng <xupeng@xupeng.me>
2015-04-01 22:25:51 +0800

    support non-lazy mode

--- cozydb/cozydb.py
+++ cozydb/cozydb.py
@@ -11,15 +11,15 @@ class CozyStore(object):
         self.kwargs = kwargs
         self._cursor = None
 
-    def get_cursor(self, use_cache=True):
+    def get_cursor(self, use_cache=True, lazy=True):
         ''' Get a cursor, return the same cursor if use_cache is True.
         '''
 
         if not use_cache:
-            return CozyCursor(*self.args, **self.kwargs)
+            return CozyCursor(lazy=lazy, *self.args, **self.kwargs)
 
         if self._cursor is None:
-            self._cursor = CozyCursor(*self.args, **self.kwargs)
+            self._cursor = CozyCursor(lazy=lazy, *self.args, **self.kwargs)
         return self._cursor
 
     def close(self):
@@ -31,7 +31,7 @@ class CozyStore(object):
 
 
 class CozyCursor(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, lazy=True, *args, **kwargs):
         ''' Accept the same arguments as MySQLdb.connect.
         '''
 
@@ -39,7 +39,11 @@ class CozyCursor(object):
         self.kwargs = kwargs
         if 'init_command' not in self.args:
             self.kwargs['init_command'] = 'set names utf8'
-        self._cursor = None
+        self._cursor = None if lazy else self._get_cursor()
+
+    def _get_cursor(self):
+        conn = MySQLdb.connect(*self.args, **self.kwargs)
+        return conn.cursor()
 
     @property
     def cursor(self):
@@ -47,8 +51,7 @@ class CozyCursor(object):
         '''
 
         if self._cursor is None:
-            conn = MySQLdb.connect(*self.args, **self.kwargs)
-            self._cursor = conn.cursor()
+            self._cursor = self._get_cursor()
         return self._cursor
 
     def __getattr__(self, name):
